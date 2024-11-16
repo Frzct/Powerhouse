2444ae9e0b6d06433068f5d5be2b0187605b61a3
Frzct <gusiccat@gmail.com>
2024-11-16 08:49:25 +0700

    public change

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("GAy!")
+const Express = require("express")
+const Path = require("path")
+const Application = Express()
+
+const quickJoin = Path.join
+const Landing = quickJoin(__dirname, "public/landing")
+
+Application.get("/", (req, res) => {
+    res.sendFile(quickJoin(Landing, "index.html"))
+})
+Application.listen(3000, () => {
+    console.log("Did you know? Those who know.")
+})
