c2a99ddca3105cc9833d586a789e9bb19dd7c5a0
Frzct <gusiccat@gmail.com>
2024-11-16 09:04:59 +0700

    i commit

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const Path = require("path")
 const Application = Express()
 
 const quickJoin = Path.join
-const Landing = quickJoin(__dirname, "public/landing")
+const Public = quickJoin(__dirname, "public")
+
+Application.use(Express.static(Public))
 
 Application.get("/", (req, res) => {
-    res.sendFile(quickJoin(Landing, "index.html"))
+    res.sendFile(quickJoin(Public, "landing/index.html"))
 })
+
 Application.listen(3000, () => {
     console.log("Did you know? Those who know.")
 })
